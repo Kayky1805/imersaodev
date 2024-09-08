fc31cb7c4e8d6c247b1d76c7afe918b5d70a7ea1
Kayky1805 <kayky1805@gmail.com>
2024-09-07 21:26:57 -0300

    ..

---
+++ dados.js
@@ -0,0 +1,28 @@
+let dados = [
+    {
+        titulo: "Os Beatles",
+        descricao: "Os Beatles foram uma banda de rock inglesa formada em Liverpool nos anos 1960. Composta por John Lennon, Paul McCartney, George Harrison e Ringo Starr, o quarteto revolucionou a música popular.",
+        informacao: '<a href="https://pt.wikipedia.org/wiki/The_Beatles" target="_blank">Saiba mais sobre Os Beatles aqui.</a>'
+    },
+    {
+        titulo: "John Lennon",
+        descricao: 'Foi um dos pilares da banda de rock mais influente de todos os tempos, os Beatles. Sua parceria com Paul McCartney na composição de músicas atemporais como "A Day In The Life", "I´ve Got A Feeling" e músicas da sua carreira solo como "Imagine" o tornou um dos compositores mais célebres da história da música.',
+        informacao: '<a href="https://pt.wikipedia.org/wiki/John_Lennon" target="_blank">Saiba mais sobre John Lennon aqui.</a>'
+    },
+    {
+        titulo: "Paul McCartney",
+        descricao: 'É um dos músicos mais influentes e bem-sucedidos de todos os tempos. Conhecido mundialmente por sua participação na banda The Beatles, ele revolucionou a música popular e continua a encantar fãs ao redor do globo.',
+        informacao: '<a href="https://pt.wikipedia.org/wiki.org/wiki/Paul_McCartney" target="_blank">Saiba mais sobre Paul McCartney aqui.</a>'
+    },
+    {
+        titulo: "George Harrison",
+        descricao: 'O mais jovem dos Beatles, era frequentemente descrito como o "Beatle silencioso". Além de suas habilidades como guitarrista, Harrison era um compositor talentoso. Músicas como "Here Comes the Sun", "Something" e "My Sweet Lord".',
+        informacao: '<a href="https://pt.wikipedia.org/wiki/George_Harrison" target="_blank">Saiba mais sobre George Harrison aqui.</a>'
+    },
+    {
+        titulo: "Ringo Starr",
+        descricao: 'Ringo Starr, o baterista dos Beatles, era muito mais do que os “Beatle tranquilo”. Seu estilo de bateria, caracterizado por um groove contagiante e um som preciso, foi fundamental para o som único da banda. Embora ele seja frequentemente considerado o membro menos “técnico” dos Beatles, sua contribuição para a música da banda foi inestimável.',
+        informacao: '<a href="https://pt.wikipedia.org.wiki/Ringo_Starr"" target="_blank">Saiba mais sobre Ringo Starr aqui.</a>'
+    }
+];
+
